ff3b4928ec67273a439f3da5deb168353687b37f
AvanthikaMaharaja <86563030+AvanthikaMaharaja@users.noreply.github.com>
2021-06-27 20:49:45 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+  <p>Copyrights by ShapeAI @ 2021{new Date().getFullYear}</p>
+  </footer>
+  ); 
+}
+
+export default Footer;
